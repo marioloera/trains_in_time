820934ceac2fbefca1e76a80616c967dba55760c
Mario Loera <mario.loera@trustly.com>
2022-03-21 12:04:49 +0100

    add train_test

--- src/tests/train_test.py
+++ src/tests/train_test.py
@@ -59,9 +59,11 @@ class TestTrain:
         assert new_train.daparture.station_code == "TPE"
 
     def test_process_timetables_empty(self):
-        new_train = Train(self.data)
-        new_train._process_timetables([])
-        assert not new_train.valid
+        self.train._process_timetables([])
+        assert not self.train.valid
+
+    def test_process_timetables_not_list(self):
+        assert self.train._process_timetables("") is None
 
     def test_estimate_arrival_time(self):
         delay_min = 3
